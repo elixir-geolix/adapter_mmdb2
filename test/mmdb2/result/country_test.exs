c37f14f75efa40306c660ff8897c2ebfac917386
Marc Neudert <marc.neudert@gmail.com>
2017-01-05 11:20:10 +0100

    imports result struct tests

---
+++ test/mmdb2/result/country_test.exs
@@ -0,0 +1,87 @@
+defmodule Geolix.Adapter.MMDB2.Result.CountryTest do
+  use ExUnit.Case, async: true
+
+  alias Geolix.Result.Country
+
+  test "result type" do
+    assert %Country{} = Geolix.lookup("2.125.160.216", where: :fixture_country)
+  end
+
+  test "locale result" do
+    result = Geolix.lookup("202.196.224.0", [ locale: :fr, where: :fixture_country ])
+
+    assert result.continent.name == result.continent.names[:fr]
+    assert result.country.name == result.country.names[:fr]
+    assert result.registered_country.name == result.registered_country.names[:fr]
+    assert result.represented_country.name == result.represented_country.names[:fr]
+  end
+
+  test "locale result (default :en)" do
+    result = Geolix.lookup("202.196.224.0", [ where: :fixture_country ])
+
+    assert result.continent.name == result.continent.names[:en]
+    assert result.country.name == result.country.names[:en]
+  end
+
+  test "ipv6 lookup" do
+    ip                  = "2001:218::"
+    { :ok, ip_address } = ip |> String.to_char_list() |> :inet.parse_address()
+
+    result = Geolix.lookup(ip, where: :fixture_country)
+
+    assert result.traits.ip_address == ip_address
+
+    assert "Asia" == result.continent.names[:en]
+    assert "Japan" == result.country.names[:en]
+    assert "Japan" == result.registered_country.names[:en]
+  end
+
+  test "regular country" do
+    ip     = { 216, 160, 83, 56 }
+    result = Geolix.lookup(ip, where: :fixture_country)
+
+    assert result.traits.ip_address == ip
+
+    assert "NA" == result.continent.code
+    assert 6255149 == result.continent.geoname_id
+    assert "Северная Америка" == result.continent.names[:ru]
+
+    assert 6252001 == result.country.geoname_id
+    assert "US" == result.country.iso_code
+    assert "アメリカ合衆国" == result.country.names[:ja]
+
+    assert 2635167 == result.registered_country.geoname_id
+    assert "GB" == result.registered_country.iso_code
+    assert "Vereinigtes Königreich" == result.registered_country.names[:de]
+  end
+
+  test "represented country" do
+    ip     = { 202, 196, 224, 0 }
+    result = Geolix.lookup(ip, where: :fixture_country)
+
+    assert result.traits.ip_address == ip
+
+    assert "AS" == result.continent.code
+    assert 6255147 == result.continent.geoname_id
+    assert "Asie" == result.continent.names[:fr]
+
+    assert 1694008 == result.country.geoname_id
+    assert "PH" == result.country.iso_code
+    assert "Filipinas" == result.country.names[:"pt-BR"]
+
+    assert result.registered_country == result.country
+
+    assert 6252001 == result.represented_country.geoname_id
+    assert "US" == result.represented_country.iso_code
+    assert "Estados Unidos" == result.represented_country.names[:es]
+    assert "military" == result.represented_country.type
+  end
+
+  test "with traits" do
+    ip     = { 67, 43, 156, 0 }
+    result = Geolix.lookup(ip, where: :fixture_country)
+
+    assert result.traits.ip_address == ip
+    assert result.traits.is_anonymous_proxy == true
+  end
+end
---
+++ test/mmdb2/result/isp_test.exs
@@ -0,0 +1,103 @@
+defmodule Geolix.Adapter.MMDB2.Result.ISPTest do
+  use ExUnit.Case, async: true
+
+  alias Geolix.Result.ISP
+
+  test "result type" do
+    assert %ISP{} = Geolix.lookup("1.0.128.0", where: :fixture_isp)
+  end
+
+  test "ipv6 lookup" do
+    ip                  = "2c0f:ff40::"
+    { :ok, ip_address } = ip |> String.to_char_list() |> :inet.parse_address()
+
+    result   = Geolix.lookup(ip, where: :fixture_isp)
+    expected = %ISP{ autonomous_system_number:       10474,
+                     autonomous_system_organization: "MWEB-10474",
+                     ip_address:                     ip_address }
+
+    assert result == expected
+  end
+
+  test "precision isp" do
+    ip       = { 128, 101, 101, 101 }
+    result   = Geolix.lookup(ip, where: :fixture_precision_isp )
+    expected = %ISP{ autonomous_system_number:       2313,
+                     autonomous_system_organization: "Precision AS Org",
+                     ip_address:                     ip,
+                     isp:                            "Precision ISP",
+                     organization:                   "Precision Org" }
+
+    assert result == expected
+  end
+
+  test "autonomous system" do
+    ip       = { 222, 230, 140, 0 }
+    result   = Geolix.lookup(ip, where: :fixture_isp)
+    expected = %ISP{ autonomous_system_number:       2519,
+                     autonomous_system_organization: "JPNIC",
+                     ip_address:                     ip }
+
+    assert result == expected
+  end
+
+  test "complete entry" do
+    ip       = { 176, 128, 0, 0 }
+    result   = Geolix.lookup(ip, where: :fixture_isp)
+    expected = %ISP{ autonomous_system_number:       12844,
+                     autonomous_system_organization: "Bouygues Telecom",
+                     ip_address:                     ip,
+                     isp:                            "Bouygues Telecom",
+                     organization:                   "Bouygues Telecom" }
+
+    assert result == expected
+  end
+
+  test "isp + organization" do
+    ip       = { 196, 12, 144, 0 }
+    result   = Geolix.lookup(ip, where: :fixture_isp)
+    expected = %ISP{ ip_address:   ip,
+                     isp:          "Rwandatel, SA",
+                     organization: "Wireless Broadband Customer" }
+
+    assert result == expected
+  end
+
+  test "missing: autonomous_system_organization" do
+    ip       = { 142, 217, 214, 0 }
+    result   = Geolix.lookup(ip, where: :fixture_isp)
+    expected = %ISP{ autonomous_system_number: 35911,
+                     ip_address:               ip,
+                     isp:                      "Telebec",
+                     organization:             "LINO Solutions Internet de Télébec" }
+
+    assert result == expected
+  end
+
+  test "only: autonomous system number" do
+    ip       = { 69, 218, 48, 0 }
+    result   = Geolix.lookup(ip, where: :fixture_isp)
+    expected = %ISP{ autonomous_system_number: 7132,
+                     ip_address:               ip }
+
+    assert result == expected
+  end
+
+  test "only: isp" do
+    ip       = { 41, 112, 0, 0 }
+    result   = Geolix.lookup(ip, where: :fixture_isp)
+    expected = %ISP{ ip_address: ip,
+                     isp:        "MTN SA" }
+
+    assert result == expected
+  end
+
+  test "only: organization" do
+    ip       = { 32, 64, 159, 0 }
+    result   = Geolix.lookup(ip, where: :fixture_isp)
+    expected = %ISP{ ip_address:   ip,
+                     organization: "AT&T Wireless" }
+
+    assert result == expected
+  end
+end
