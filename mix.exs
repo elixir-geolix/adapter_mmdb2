a9c02f5e33bebaf72270d7b086c405d800acd641
Marc Neudert <marc.neudert@gmail.com>
2017-01-08 13:01:37 +0100

    pins :excoveralls to "< 0.6.0"

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Geolix.Adapter.MMDB2.Mixfile do
   defp deps do
     [ { :ex_doc, ">= 0.0.0", only: :dev },
 
-      { :excoveralls, "~> 0.5",  only: :test },
+      { :excoveralls, "< 0.6.0",  only: :test },
       { :geolix,      "~> 0.11", only: :test } ]
   end
 
