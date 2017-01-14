9961657f470e34d0b40ff365e4f1f60774fe042d
Marc Neudert <marc.neudert@gmail.com>
2017-01-14 16:26:48 +0100

    prevents "undefined or private" warning

--- test/fixtures/download.ex
+++ test/fixtures/download.ex
@@ -20,15 +20,16 @@ defmodule Geolix.Adapter.MMDB2TestFixtures.Download do
     end
   end
 
-  defp download_fixture(remote, local) do
-    content = case Version.match?(System.version, ">= 1.1.0") do
-      false -> Mix.Utils.read_path!(remote)
-      true  ->
-        { :ok, data } = Mix.Utils.read_path(remote)
-        data
-    end
+  if Version.match?(System.version, ">= 1.1.0") do
+    defp download_fixture(remote, local) do
+      { :ok, content } = Mix.Utils.read_path(remote)
 
-    File.write! local, content
+      File.write! local, content
+    end
+  else
+    defp download_fixture(remote, local) do
+      File.write! local, Mix.Utils.read_path!(remote)
+    end
   end
 
   defp local(filename) do
