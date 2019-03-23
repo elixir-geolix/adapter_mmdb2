dbef47390fe9fe075da171df89efea2fa9c3eeb5
Marc Neudert <marc.neudert@gmail.com>
2019-03-23 12:08:41 +0100

    fixes verification dependencies

--- verify/geolix/mix.exs
+++ verify/geolix/mix.exs
@@ -6,19 +6,9 @@ defmodule Geolix.Verification.Mixfile do
       app: :geolix_verification,
       version: "0.0.1",
       elixir: "~> 1.5",
-      deps: deps(),
+      deps: [{:geolix_adapter_mmdb2, path: "../../"}],
       deps_path: "../../deps",
       lockfile: "../../mix.lock"
-    ]
-  end
-
-  def application, do: [applications: [:geolix]]
-
-  defp deps do
-    [
-      {:geolix, "~> 0.17"},
-      {:geolix_adapter_mmdb2, path: "../../"},
-      {:mmdb2_decoder, "~> 1.0", override: true}
     ]
   end
 end
