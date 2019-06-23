1e2d2d0043a606fe9f6134db09a6f1321df3680b
Marc Neudert <marc.neudert@gmail.com>
2019-06-23 13:20:28 +0200

    Use separate storage process for each database

--- lib/mmdb2/storage.ex
+++ lib/mmdb2/storage.ex
@@ -7,25 +7,41 @@ defmodule Geolix.Adapter.MMDB2.Storage do
 
   @type storage_entry :: {Metadata.t() | nil, binary | nil, binary | nil}
 
+  @doc """
+  Returns the worker specification for a database storage process.
+  """
+  @spec worker(atom) :: Supervisor.Spec.spec()
+  def worker(database_id) do
+    worker_id = worker_id(database_id)
+
+    Supervisor.Spec.worker(__MODULE__, [worker_id], id: worker_id)
+  end
+
   @doc false
-  @spec start_link(map) :: Agent.on_start()
-  def start_link(initial_value \\ %{}) do
-    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  @spec start_link(atom) :: Agent.on_start()
+  def start_link(worker_id) do
+    Agent.start_link(fn -> nil end, name: worker_id)
   end
 
   @doc """
   Fetches the data for a database.
   """
   @spec get(atom) :: storage_entry | nil
-  def get(database) do
-    Agent.get(__MODULE__, &Map.get(&1, database, nil))
+  def get(database_id) do
+    database_id
+    |> worker_id()
+    |> Agent.get(fn entry -> entry end)
   end
 
   @doc """
   Stores the data for a database.
   """
   @spec set(atom, storage_entry) :: :ok
-  def set(database, entry) do
-    Agent.update(__MODULE__, &Map.put(&1, database, entry))
+  def set(database_id, entry) do
+    database_id
+    |> worker_id()
+    |> Agent.update(fn _ -> entry end)
   end
+
+  defp worker_id(database_id), do: :"geolix_mmdb2_storage_#{database_id}"
 end
