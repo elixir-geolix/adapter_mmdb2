d3be477cdf3525bd81753410337423bdcfda7b04
Marc Neudert <marc.neudert@gmail.com>
2020-04-10 13:15:24 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule Geolix.Adapter.MMDB2.MixProject do
     [
       {:benchee, "~> 1.0", only: :bench, runtime: false},
       {:credo, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.12", only: :test, runtime: false},
       {:geolix, "~> 1.0"},
