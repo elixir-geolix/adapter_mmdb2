34854071ef283cdc0f6fe04fa5ebea75e0e48297
Marc Neudert <marc.neudert@gmail.com>
2020-05-18 19:57:38 +0200

    Clean up test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,48 +1,41 @@
 alias Geolix.Adapter.MMDB2
 alias Geolix.Adapter.MMDB2TestHelpers.Fixture
 
-{:ok, _} = Application.ensure_all_started(:geolix)
-
 Fixture.download()
 
-fixtures =
-  Fixture.list()
-  |> Enum.map(fn
-    {id, filename, :autoload} ->
-      source = Path.join([__DIR__, "fixtures", filename])
+Fixture.list()
+|> Enum.map(fn
+  {id, filename, :autoload} ->
+    %{id: id, adapter: MMDB2, source: Path.join(Fixture.path(), filename)}
 
-      %{id: id, adapter: MMDB2, source: source}
+  _ ->
+    nil
+end)
+|> Enum.reject(&(&1 == nil))
+|> Enum.each(&Geolix.load_database/1)
 
-    _ ->
-      nil
-  end)
-  |> Enum.reject(&(&1 == nil))
-
-databases =
-  [
-    %{
-      id: :testdata_gz,
-      adapter: MMDB2,
-      source: Path.join(Geolix.TestData.dir(:mmdb2), "Geolix.mmdb.gz")
-    },
-    %{
-      id: :testdata_plain,
-      adapter: MMDB2,
-      source: Path.join(Geolix.TestData.dir(:mmdb2), "Geolix.mmdb")
-    },
-    %{
-      id: :testdata_tar,
-      adapter: MMDB2,
-      source: Path.join(Geolix.TestData.dir(:mmdb2), "Geolix.mmdb.tar")
-    },
-    %{
-      id: :testdata_targz,
-      adapter: MMDB2,
-      source: Path.join(Geolix.TestData.dir(:mmdb2), "Geolix.mmdb.tar.gz")
-    }
-  ] ++ fixtures
-
-Application.put_env(:geolix, :databases, databases)
-Enum.each(databases, &Geolix.load_database/1)
+[
+  %{
+    id: :testdata_gz,
+    adapter: MMDB2,
+    source: Path.join(Geolix.TestData.dir(:mmdb2), "Geolix.mmdb.gz")
+  },
+  %{
+    id: :testdata_plain,
+    adapter: MMDB2,
+    source: Path.join(Geolix.TestData.dir(:mmdb2), "Geolix.mmdb")
+  },
+  %{
+    id: :testdata_tar,
+    adapter: MMDB2,
+    source: Path.join(Geolix.TestData.dir(:mmdb2), "Geolix.mmdb.tar")
+  },
+  %{
+    id: :testdata_targz,
+    adapter: MMDB2,
+    source: Path.join(Geolix.TestData.dir(:mmdb2), "Geolix.mmdb.tar.gz")
+  }
+]
+|> Enum.each(&Geolix.load_database/1)
 
 ExUnit.start()
