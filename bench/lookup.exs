a1d24fc438c91d6d3a63d80557543b936d77bd28
Marc Neudert <marc.neudert@gmail.com>
2020-05-21 10:44:07 +0200

    Remove module wrapping from benchmark

--- bench/lookup.exs
+++ bench/lookup.exs
@@ -1,29 +1,23 @@
-defmodule Geolix.Adapter.MMDB2.Benchmark.Lookup do
-  def run do
-    :ok =
-      Geolix.load_database(%{
-        id: :benchmark,
-        adapter: Geolix.Adapter.MMDB2,
-        source: Geolix.TestData.file(:mmdb2, "Benchmark.mmdb")
-      })
+:ok =
+  Geolix.load_database(%{
+    id: :benchmark,
+    adapter: Geolix.Adapter.MMDB2,
+    source: Geolix.TestData.file(:mmdb2, "Benchmark.mmdb")
+  })
 
-    {:ok, lookup_ipv4} = :inet.parse_address('1.1.1.1')
-    {:ok, lookup_ipv4_in_ipv6} = :inet.parse_address('::1.1.1.1')
+{:ok, lookup_ipv4} = :inet.parse_address('1.1.1.1')
+{:ok, lookup_ipv4_in_ipv6} = :inet.parse_address('::1.1.1.1')
 
-    Benchee.run(
-      %{
-        "IPv4 in IPV6 lookup" => fn ->
-          Geolix.lookup(lookup_ipv4_in_ipv6, where: :benchmark)
-        end,
-        "IPv4 lookup" => fn ->
-          Geolix.lookup(lookup_ipv4, where: :benchmark)
-        end
-      },
-      formatters: [{Benchee.Formatters.Console, comparison: false}],
-      warmup: 2,
-      time: 10
-    )
-  end
-end
-
-Geolix.Adapter.MMDB2.Benchmark.Lookup.run()
+Benchee.run(
+  %{
+    "IPv4 in IPV6 lookup" => fn ->
+      Geolix.lookup(lookup_ipv4_in_ipv6, where: :benchmark)
+    end,
+    "IPv4 lookup" => fn ->
+      Geolix.lookup(lookup_ipv4, where: :benchmark)
+    end
+  },
+  formatters: [{Benchee.Formatters.Console, comparison: false}],
+  warmup: 2,
+  time: 10
+)
